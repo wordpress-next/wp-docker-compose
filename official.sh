c812bebfd50d9a169902fbd63e721460ccd33666
Hill <hill@kimo.com>
2021-09-12 17:18:50 +0800

    update official cofigure

--- official.sh
+++ official.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-env $(cat .env-official) docker-compose -f docker-compose-official-wp.yml $* 
+ENV_FILE=.env-official
+
+if [ ! -e "$ENV_FILE" ]; then
+  echo -n "
+    config file [$ENV_FILE] not found
+
+    You could run
+      cp .env.official.sample $ENV_FILE
+    to gen default config file
+
+  "
+  exit;
+fi
+
+env $(cat $ENV_FILE) docker-compose -f docker-compose-official-wp.yml $* 
